b00b1004b756925869c7cc6e1aef2f0cac6284b4
Abel Kalum <chumbabel@gmail.com>
2020-03-29 00:21:47 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Abe', 3, 'Male', 'Black', '');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Abe', 3, 'Male', 'Black', 'Sanguine', );
