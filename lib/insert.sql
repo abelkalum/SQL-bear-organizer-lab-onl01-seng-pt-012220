4326f6b8d5f551411cffa99741ee3072bfc45282
Abel Kalum <chumbabel@gmail.com>
2020-03-29 00:28:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'Male', 'Black', 'Sanguine', 'Yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'Male', 'Black', 'Choleric', 'Yes');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Caleb', 3, 'Male', 'Brown', 'Sanguine', 'Yes');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3, 'Female', 'Brown', 'Sanguine', 'Yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Dan', 3, 'Male', 'Black', 'Melancholy', 'Yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Eevee', 3, 'Female', 'Brown', 'Sanguine', 'Yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Fiona', 3, 'Female', 'Black', 'Phlegmatic', 'Yes');
