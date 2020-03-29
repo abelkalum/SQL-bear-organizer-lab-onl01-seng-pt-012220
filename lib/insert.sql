abfa8f0f045b742d90673daa7fc04a7b366fb0de
Abel Kalum <chumbabel@gmail.com>
2020-03-29 00:15:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,1 @@
-INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
-INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
-INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
-INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
-INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Maru', 3, 'Scottish Fold');
