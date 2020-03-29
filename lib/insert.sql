a21fbd5323bd2f4685b4fed74d233252bed8271e
Abel Kalum <chumbabel@gmail.com>
2020-03-29 00:45:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Black", "Sanguine", "Yes");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 7, "Male", "Black", "Choleric", "Yes");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "Female", "Brown", "Sanguine", "Yes");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1, "Male", "Black", "Melancholy", "Yes");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "Female", "Brown", "Sanguine", "Yes");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "Male", "Black", "Phlegmatic", "Yes");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 5, "Female", "Black", "Sanguine", "Yes");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, , "Male", "Black", "Sanguine", "No");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "Male", "Black", "Sanguine", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 7, "Male", "Black", "Choleric", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "Female", "Brown", "Sanguine", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1, "Male", "Black", "Melancholy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "Female", "Brown", "Sanguine", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "Male", "Black", "Phlegmatic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 5, "Female", "Black", "Sanguine", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, , "Male", "Black", "Sanguine", 0);
