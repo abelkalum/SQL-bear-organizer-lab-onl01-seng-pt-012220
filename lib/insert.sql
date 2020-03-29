64c25576e966047662561d683d304c761cb5dc15
Abel Kalum <chumbabel@gmail.com>
2020-03-29 00:25:39 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Abe', 3, 'Male', 'Black', 'Sanguine', 'Yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Bob', 2, 'Male', 'Black', 'Choleric', 'Yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Caleb', 3, 'Male', 'Brown', 'Sanguine', 'Yes');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Dan', 3, 'Male', 'Black', 'Sanguine', 'Yes');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Abe', 3, 'Male', 'Black', 'Sanguine', 'Yes');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Dan', 3, 'Male', 'Black', 'Melancholy', 'Yes');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Eevee', 3, 'Female', 'Brown', 'Sanguine', 'Yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Abe', 3, 'Male', 'Black', 'Sanguine', 'Yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Abe', 3, 'Male', 'Black', 'Sanguine', 'Yes');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Abe', 3, 'Male', 'Black', 'Sanguine', 'Yes');
