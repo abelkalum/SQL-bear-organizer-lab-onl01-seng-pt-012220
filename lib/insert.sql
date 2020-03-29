322730a0473a6e5827d633b26214ad956ea1e3cc
Abel Kalum <chumbabel@gmail.com>
2020-03-29 00:14:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,3 @@
 INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
 INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
